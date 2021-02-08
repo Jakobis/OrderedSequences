5ef03cb4d8941212bb32f7c17ecdbad01f73f0ff
Jakob Uttenthal Israelsen <jais@itu.dk>
2021-02-08 14:34:52 +0100

    interpreter now works properly

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,10 @@
-import filecmp
+import filecmp, os
 
 def interpret(test, sequence):
     inFile = open(f'tests/{test}.in', 'r')
     outName = ".testoutput"
     outFile = open(outName, 'w')
+    write = lambda x: outFile.write(f'{x}\n')
     lines = inFile.readlines()
     for line in lines:
         parts = line.split()
@@ -14,12 +15,19 @@ def interpret(test, sequence):
         if parts[0] == "re":
             sequence.remove(sequence, int(parts[1]))
         if parts[0] == "r":
-            outFile.write(sequence.rank(sequence, int(parts[1])))
+            write(sequence.rank(sequence, int(parts[1])))
         if parts[0] == "s":
-            outFile.write(sequence.select(sequence, int(parts[1])))
+            write(sequence.select(sequence, int(parts[1])))
         if parts[0] == "sf":
-            outFile.write(sum(sequence.iter(sequence)))
+            write(sum(sequence.iter(sequence)))
         if parts[0] == "sr":
-            outFile.write(sum(sequence.reversed(sequence)))
+            write(sum(sequence.reversed(sequence)))
     outFile.close()
-    return filecmp.cmp(f'tests/{test}.out', outName)
+    res = filecmp.cmp(f'tests/{test}.out', outName)
+    if not res:
+        outFile = open(outName, 'r')
+        answerFile = open(f'tests/{test}.out', 'r')
+        print(outFile.readlines())
+        print(answerFile.readlines())
+    os.remove(outName)
+    return res
