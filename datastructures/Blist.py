0a7fcb39a3268d842bdc1876e9eeb0a745096fb2
Hjalte Dalland <hjda@itu.dk>
2021-02-08 13:55:03 +0100

    add blist

---
+++ datastructures/Blist.py
@@ -0,0 +1,22 @@
+from datastructures.Template import Template
+from blist import sortedlist
+
+class Blist(Template):
+    def __init__(self):
+        self.li = sortedlist()
+    def add(self, element):
+        self.li.add(element)
+    def delete(self, index):
+        self.li.pop(index)
+    def remove(self, element):
+        self.li.remove(element)
+    def rank(self, element):
+        return self.li.index(element)
+    def select(self, index):
+        return self.li[index]
+    def iter(self):
+        return iter(self.li)
+    def reversed(self):
+        return self.li.__reversed__()
+    def count(self, value):
+        return self.li.count(value)
