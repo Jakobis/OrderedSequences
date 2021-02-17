ad91c4c7a067e0b099acc8f7171b45fb244d099d
Hjalte Dalland <hjda@itu.dk>
2021-02-17 13:48:05 +0100

    Make plotting better

--- interpreter.py
+++ interpreter.py
@@ -44,7 +44,7 @@ def interpret(test, sequence, benches = {}, testinterval = 999999999):
     count = 1
     for line in lines[1:]:
         if count % testinterval == 0: 
-            start_time = time.perf_counter()
+            start_time = time.process_time_ns()
        
         parts = line.split()
         if parts[0] == "a":
@@ -65,7 +65,7 @@ def interpret(test, sequence, benches = {}, testinterval = 999999999):
             write(sequence.predecessor(sequence, int(parts[1])))
         
         if count % testinterval == 0: 
-            end_time = time.perf_counter()
+            end_time = time.process_time_ns()
             finding = [start_time, end_time, end_time-start_time, sequence.size(sequence)]
             benches[parts[0]].append(finding)
         count += 1
