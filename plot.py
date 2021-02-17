fa8c354e81f72f6d4e6133c97e95115b66322bf8
Hjalte Dalland <hjda@itu.dk>
2021-02-17 16:14:10 +0100

    Rolling median instead

--- plot.py
+++ plot.py
@@ -3,22 +3,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
 
+def mad(data, axis=None):
+    return np.mean(np.absolute(data - np.mean(data, axis)), axis)
+
 def dfcsv(name):
     data = pd.read_csv(f"timings/{name}.medium.csv")
     df = pd.DataFrame(data,columns=['Time(s)','Size'])
     df[(np.abs(stats.zscore(df['Time(s)'])) < 3)]
-    df['mean'] = df['Time(s)'].rolling(100).mean()
+    df['median'] = df['Time(s)'].rolling(1000).median()
+    # Below line removes outliers but is not necessary
+    #df['std'] = df['Time(s)'].rolling(10).std()
+    #df = df[(df['Time(s)'] <= df['median']+0.5*df['std']) & (df['Time(s)'] >= df['median']-3*df['std'])] 
     return df
 
 def addplot(name, ax):
     df = dfcsv(name)
-    df.plot(x ='Size', y='mean', kind = 'line', ax = ax, label=name)
+    df.plot(x ='Size', y='median', kind = 'line', ax = ax, label=name)
 
 df = dfcsv("Blist")
-ax = df.plot(x ='Size', y='mean', kind = 'line', label="Blist")	
+ax = df.plot(x ='Size', y='median', kind = 'line', label="Blist")	
 
 addplot("SortedContainer", ax)
-#addplot("Basic", ax)
+addplot("Basic", ax)
 addplot("Skiplist", ax)
 addplot("RBSTree", ax)
 addplot("SortedCollection", ax)
