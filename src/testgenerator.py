7c32806c481894a29409b2dba4e109e021987eef
Hjalte Dalland <hjda@itu.dk>
2021-02-22 13:01:52 +0100

    Better test generator

--- src/testgenerator.py
+++ src/testgenerator.py
@@ -2,75 +2,81 @@ import sys, os
 from sortedcontainers import SortedList
 import random
 
+MININT = 0
+MAXINT = 2147483647
 
+def generate_testcase(n, ops):
+    test_input = []
+    test_answer = []
+    test_input.append(f"{n}")
+    current_list = SortedList()
+    for op in ops:
+        if op == "add" or len(current_list) < 2:
+            value = random.randint(MININT, MAXINT)
+            current_list.add(value)
+            test_input.append(f"a {value}")
 
-if len(sys.argv) != 3:
-    print("Please input number of lines and testname")
-    exit(-1)
-n = int(sys.argv[1])
-filename = sys.argv[2].strip()
-infile = open(f"../tests/{filename}.in", "w")
-outfile = open(f"../tests/{filename}.out", "w")
+        elif op == "delete":
+            index = random.randint(0, len(current_list) - 1)
+            current_list.pop(index)
+            test_input.append(f"d {index}")
 
-operations = ["add", "delete", "remove", "rank", "select", "sum", "successor", "predecessor"]
-weights    = (   50,       10,       10,     20,       30,    10,          15,           15)
+        elif op == "remove":
+            index = random.randint(0, len(current_list) - 1)
+            value = current_list.pop(index)
+            test_input.append(f"re {value}")
 
-minint = 0
-maxint = 2147483647
+        elif op == "rank":
+            index = random.randint(0, len(current_list) - 1)
+            value = current_list[index]
+            test_input.append(f"r {value}")
+            test_answer.append(f"{index}")
 
-current_list = SortedList()
+        elif op == "select":
+            index = random.randint(0, len(current_list) - 1)
+            test_input.append(f"s {index}")
+            test_answer.append(f"{current_list[index]}")
+
+        elif op == "sum":
+            test_input.append(f"sum")
+            test_answer.append(f"{sum(current_list)}")
+
+        elif op == "successor":
+            index = random.randint(0, len(current_list) - 2)
+            value = current_list[index]
+            test_input.append(f"su {value}")
+            test_answer.append(f"{current_list[index + 1]}")
+
+        elif op == "predecessor":
+            index = random.randint(1, len(current_list) - 1)
+            value = current_list[index]
+            test_input.append(f"pr {value}")
+            test_answer.append(f"{current_list[index - 1]}")
+    return (test_input, test_answer)
+
 
 def randomchoice(i):
+    operations = ["add", "delete", "remove", "rank", "select", "sum", "successor", "predecessor"]
+    weights    = (   50,       10,       10,     20,       30,    10,          15,           15)
     return random.choices(operations, weights)[0]
 
-def testadd(i):
-    return "add"
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Please input number of lines, testname and test type (random, add)")
+        exit(-1)
+    n = int(sys.argv[1])
+    filename = sys.argv[2].strip()
+    ops = []
+    testtype = sys.argv[3].strip()
+    if testtype == "random":
+        ops = [randomchoice(i) for i in range(n)]
+    elif testtype == "add":
+        ops = ["add" for i in range(n)]
+    test_input, test_answer = generate_testcase(n, ops)
+    infile = open(f"../tests/{filename}.in", "w")
+    outfile = open(f"../tests/{filename}.out", "w")
+    infile.write("\n".join(test_input))
+    outfile.write("\n".join(test_answer))
+    infile.close()
+    outfile.close()
 
-
-infile.write(f"{n}\n")
-for i in range(n):
-    op = testadd(i)
-    if op == "add" or len(current_list) < 2:
-        value = random.randint(minint, maxint)
-        current_list.add(value)
-        infile.write(f"a {value}\n")
-
-    elif op == "delete":
-        index = random.randint(0, len(current_list) - 1)
-        current_list.pop(index)
-        infile.write(f"d {index}\n")
-
-    elif op == "remove":
-        index = random.randint(0, len(current_list) - 1)
-        value = current_list.pop(index)
-        infile.write(f"re {value}\n")
-
-    elif op == "rank":
-        index = random.randint(0, len(current_list) - 1)
-        value = current_list[index]
-        infile.write(f"r {value}\n")
-        outfile.write(f"{index}\n")
-
-    elif op == "select":
-        index = random.randint(0, len(current_list) - 1)
-        infile.write(f"s {index}\n")
-        outfile.write(f"{current_list[index]}\n")
-
-    elif op == "sum":
-        infile.write(f"sum\n")
-        outfile.write(f"{sum(current_list)}\n")
-
-    elif op == "successor":
-        index = random.randint(0, len(current_list) - 2)
-        value = current_list[index]
-        infile.write(f"su {value}\n")
-        outfile.write(f"{current_list[index + 1]}\n")
-
-    elif op == "predecessor":
-        index = random.randint(1, len(current_list) - 1)
-        value = current_list[index]
-        infile.write(f"pr {value}\n")
-        outfile.write(f"{current_list[index - 1]}\n")
-
-infile.close()
-outfile.close()
