e3594d96f83c5e4875378a3021843ac2acfddaf9
Hjalte Dalland <hjda@itu.dk>
2021-03-08 14:10:43 +0100

    premake selects for all, so its fairer and faster.

--- src/benchmarkingv2.py
+++ src/benchmarkingv2.py
@@ -1,6 +1,7 @@
 from datastructures import structures
 import random, time, gc, threading
 import traceback
+import numpy as np
 MININT = -2147483648
 MAXINT = 2147483647
 
@@ -45,7 +46,7 @@ def run_benchmark():
             # We don't have a good way of measuring these under small n
             # So results which ran faster than a threshold will get discarded
             # As the measurement error will be too large.
-            preload_values = [random.randint(MININT, MAXINT) for i in range(10**n)]
+            preload_values = np.random.randint(MININT, MAXINT, 10**n)
             delete_values = [random.randint(0, i - 2) for i in range(2, 10**n + 2)] #Potentially move these into functions so they can be garbage cleaned and save ram. Might be a bit much right now
             for ds in structures:
                 instance = init_structure(ds, preload_values)
@@ -76,7 +77,7 @@ def run_benchmark():
                     print(f"{ds} was too slow and was skipped!")
                     continue
                 except Exception as e:
-                    print(f"{ds} Had an exception doing deletion:\n{e}")
+                    print(f"{ds} Had an exception doing removal:\n{e}")
                     traceback.print_exc()
                     continue
 
@@ -98,20 +99,22 @@ def run_benchmark():
             ### NON-DESTRUCTIVE ####
             # These methods should not modify the data structure
             # and can therefore be called until they took at least 1 second to run.
+            select_values =  [np.random.randint(0, 10**n -1) for i in range(4194304)] # 4 million should be enough
             for ds in structures:
-                select_values = [random.randint(0, 10**n -1) for i in range(64)]
+                selects = 64
                 instance = init_structure(ds, preload_values)
                 time_taken = 0
                 try:
                     while time_taken < 1:
-                        time_taken = time_execution(instance, instance.select, select_values)
+                        time_taken = time_execution(instance, instance.select, select_values[:selects])
                         if time_taken < 1:
-                            select_values = [random.randint(0, 10**n -1) for i in range(len(select_values) * 2)]
+                            selects *= 2
                 except TimeoutError as e:
                     print(f"{ds} was too slow and was skipped!")
                     continue
-                print(f"{ds} took {time_taken}s for selecting {len(select_values)} value")
-                write_csv_results(ds, 10**n, "Select", len(select_values), time_taken)
+                print(f"{ds} took {time_taken}s for selecting {selects} value")
+                write_csv_results(ds, 10**n, "Select", selects, time_taken)
+            del select_values
         
             f.flush()
             gc.collect()
