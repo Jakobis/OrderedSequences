de44cf5cf0187aaf45767b6e1b03301d17a8b355
Hjalte Mac Dalland - Laptop <hjaltesorgenfrei@gmail.com>
2021-04-26 13:39:25 +0200

    Add more test

--- src/cache/CacheTest.py
+++ src/cache/CacheTest.py
@@ -1,8 +1,9 @@
 import blist
 import sortedcontainers
 import sys
+from pyskiplist import SkipList
 sys.path.append('..')
-from datastructures import SortedArray
+from datastructures import SortedArray, AutoLoad
 
 N = int(sys.argv[2])
 
@@ -12,21 +13,39 @@ elif sys.argv[1] == 'SortedArray':
     l = SortedArray._SortedArray('q', [i for i in range(10**N)])
 elif sys.argv[1] == 'SortedList':
     l = sortedcontainers.SortedList([i for i in range(10**N)])
+elif sys.argv[1] == 'AutoLoad':
+    l = AutoLoad.SortedList([i for i in range(10**N)])
+elif sys.argv[1] == 'SkipList':
+    l = SkipList()
+    for i in range(10**N): 
+        l.insert(i, i)
 else:
     print("Error, did not supply working thingy")
     exit(-1)
 
-print(f"Testing {type(l).__name__}")
+print(f"Testing {sys.argv[1]}")
 print(len(l))
 if sys.argv[3] == 'Base':
     exit(0)
 elif sys.argv[3] == 'Add':
-    for i in range(10**4):
-        l.add(i)
+    if sys.argv[1] == 'SkipList':
+        for i in range(10**4):
+            l.insert(i, i)
+    else:
+        for i in range(10**4):
+            l.add(i)
 elif sys.argv[3] == 'Select':
-    ans = 0
-    for i in range(10**4):
-        ans += l[i]
+    if sys.argv[1] == 'SkipList':
+        ans = 0
+        for i in range(10**4):
+            ans += l[i][0]
+    else:
+        ans = 0
+        for i in range(10**4):
+            ans += l[i]
+elif sys.argv[3] == 'Delete':
+    for i in range(10**4 - 1, 0, -1):
+        del l[i]
 else:
     print("Error, did not supply operation")
     exit(-1)
--- src/cache/experiment.py
+++ src/cache/experiment.py
@@ -1,15 +1,18 @@
 import os
 import subprocess
-
-f = open(f"../../results/cache/cache.csv", "w")
+fileexisted = os.path.isfile(f"../../results/cache/cache.csv")
+f = open(f"../../results/cache/cache.csv", "a")
 def write_csv_results(ds, n, op, num_ops, cache, misses):
     f.write(f"{ds},{n},{op},{num_ops},{cache},{misses}\n")
     f.flush()
-write_csv_results("DS", "Size", "Op", "OpCount", "Cache", "Misses")
+if not fileexisted:
+    write_csv_results("DS", "Size", "Op", "OpCount", "Cache", "Misses")
 
-for d in ['SortedList', 'SortedArray', 'Blist']:
+for d in ['SortedList', 'SortedArray', 'Blist', 'AutoLoad', 'SkipList']:
     for i in range(4, 8):
-        for o in ['Base', 'Add', 'Select']:
+        for o in ['Base', 'Add', 'Select', 'Delete']:
+            if os.path.isfile(f"../../results/cache/{d}.{o}.{10 ** i}"):
+                continue
             os.system(f"valgrind --tool=cachegrind --cachegrind-out-file=../../results/cache/{d}.{o}.{10 ** i} python3.7 CacheTest.py {d} {i} {o}")
             if o != 'Base':
                 os.system(f"cg_diff ../../results/cache/{d}.Base.{10 ** i} ../../results/cache/{d}.{o}.{10 ** i} > ../../results/cache/{d}.{o}.{10 ** i}.Diff")
